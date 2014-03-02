5238ab8ac238dc5c99af304db5e9a02773eb4ed3
David Pullar <david-pullar@hotmail.com>
2014-03-02 14:58:15 -0500

    template

---
+++ TipperAssignment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipperAssignment
+//
+//  Created by David Pullar on 3/2/2014.
+//  Copyright (c) 2014 David Pullar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipperAssignment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipperAssignment
+//
+//  Created by David Pullar on 3/2/2014.
+//  Copyright (c) 2014 David Pullar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
